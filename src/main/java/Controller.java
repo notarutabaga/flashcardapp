487dd1ff53fedcdc944cde5fa4d2da9ea0a6959f
notarutabaga <abriannalynn0027@gmail.com>
2022-01-18 03:55:26 -0500

    game progesses based on probability

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -13,6 +13,7 @@ public class Controller {
     public Pane questionPane;
     public TextArea cardQuestion;
     public JFXButton blueButton;
+    public int deckChoice;
 
     public Pane answerPane;
     public TextArea cardAnswer;
@@ -37,12 +38,17 @@ public class Controller {
         questionPane.setVisible(true);
         redCards = Card.makeFlashcards();
         probability = Card.getProbability();
+        yellowCards = new ArrayList<>();
+        greenCards = new ArrayList<>();
 
         setQuestion();
     }
 
     public void setQuestion() {
-        int deckChoice = probability.get((int) (Math.random() * probability.size()));
+        questionPane.setVisible(true);
+        answerPane.setVisible(false);
+
+        deckChoice = probability.get((int) (Math.random() * probability.size()));
         System.out.println(deckChoice);
 
         if(deckChoice == 3) {
@@ -63,5 +69,144 @@ public class Controller {
         answerPane.setVisible(true);
 
         cardAnswer.setText(currentCard.answer);
+    }
+
+    public void clickedGreen() {
+        if(!probability.contains(1)) {
+            probability.add(1);
+        }
+
+        //if card came from green and chose green remove entirely
+        if(deckChoice == 1) {
+            greenCards.remove(currentCard);
+            if(greenCards.isEmpty()) {
+                probability.remove((Integer) 1);
+            }
+        }
+
+        //if card came from yellow and chose green put to green
+        if(deckChoice == 2) {
+            yellowCards.remove(currentCard);
+            greenCards.add(currentCard);
+
+            if(yellowCards.isEmpty()) {
+                probability.remove((Integer) 2);
+                probability.remove((Integer) 2);
+            }
+
+            if(!probability.contains(1)) {
+                probability.add(1);
+            }
+        }
+
+        //if card came from red and chose green put to green
+        if(deckChoice == 3) {
+            redCards.remove(currentCard);
+            greenCards.add(currentCard);
+
+            if(redCards.isEmpty()) {
+                probability.remove((Integer) 3);
+                probability.remove((Integer) 3);
+                probability.remove((Integer) 3);
+            }
+
+            if(!probability.contains(1)) {
+                probability.add(1);
+            }
+        }
+
+        if(greenCards.isEmpty() && yellowCards.isEmpty() && redCards.isEmpty()) {
+            //endGame();
+        } else {
+            setQuestion();
+        }
+    }
+
+    public void clickedYellow() {
+        if(!probability.contains(2)) {
+            probability.add(2);
+            probability.add(2);
+        }
+
+        //if card came from green and chose yellow put to yellow
+        if(deckChoice == 1) {
+            greenCards.remove(currentCard);
+            yellowCards.add(currentCard);
+
+            if(greenCards.isEmpty()) {
+                probability.remove((Integer) 1);
+            }
+
+            if(!probability.contains(2)) {
+                probability.add(2);
+                probability.add(2);
+            }
+        }
+
+        //if card came from yellow and chose yellow keep yellow
+
+        //if card came from red and chose yellow put to yellow
+        if(deckChoice == 3) {
+            redCards.remove(currentCard);
+            yellowCards.add(currentCard);
+
+            if(redCards.isEmpty()) {
+                probability.remove((Integer) 3);
+                probability.remove((Integer) 3);
+                probability.remove((Integer) 3);
+            }
+
+            if(!probability.contains(2)) {
+                probability.add(2);
+                probability.add(2);
+            }
+        }
+
+        if(greenCards.isEmpty() && yellowCards.isEmpty() && redCards.isEmpty()) {
+            //endGame();
+        } else {
+            setQuestion();
+        }
+    }
+
+    public void clickedRed() {
+        //if card came from green and chose red put to yellow?
+        if(deckChoice == 1) {
+            greenCards.remove(currentCard);
+            yellowCards.add(currentCard);
+
+            if(greenCards.isEmpty()) {
+                probability.remove((Integer) 1);
+            }
+
+            if(!probability.contains(2)) {
+                probability.add((Integer) 2);
+                probability.add((Integer) 2);
+            }
+        }
+
+        //if card came from yellow and chose red put to red
+        if(deckChoice == 2) {
+            yellowCards.remove(currentCard);
+            redCards.add(currentCard);
+
+            if(yellowCards.isEmpty()) {
+                probability.remove((Integer) 2);
+                probability.remove((Integer) 2);
+            }
+
+            if(!probability.contains(3)) {
+                probability.add(3);
+                probability.add(3);
+                probability.add(3);
+            }
+        }
+        //if card came from red and chose red keep red
+
+        if(greenCards.isEmpty() && yellowCards.isEmpty() && redCards.isEmpty()) {
+            //endGame();
+        } else {
+            setQuestion();
+        }
     }
 }
